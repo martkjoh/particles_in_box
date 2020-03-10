384e6e9565e38c56c784fac7e8611241deef5aaa
Martin Johnsrud <martin.johnsrud@gmail.com>
2020-03-10 15:08:14 +0100

    made potential functions more usefull

--- physics.py
+++ physics.py
@@ -9,18 +9,18 @@ K = 10
 def dot(xi, xj):
     return np.einsum("...jk, ...jk -> ...k", xi, xj)
 
-def V_w(x0):
-    r = sqrt(dot(x0, x0))
+def V_w(x):
+    r = sqrt(dot(x[0], x[0]))
     indx = r>R
     E = np.zeros_like(r)
     E[indx] = K/2 * (r[indx] - R)**2
     return E
 
-def V_i(k, x0):
-    x_kj = np.zeros_like(x0)
-    N = len(x0[0])
+def V_i(k, x):
+    x_kj = np.zeros_like(x[0])
+    N = len(x[0, 0])
     for i in range(N):
-        x_kj[:, i] = x0[:, k] - x0[:, i]
+        x_kj[:, i] = x[0, :, k] - x[0, :, i]
     r_kj = sqrt(dot(x_kj, x_kj))
     indx = r_kj<a
     indx[k] = False
@@ -28,11 +28,14 @@ def V_i(k, x0):
     E = eps*(ar**12 - 2*ar**6 + 1)
     return np.sum(E)
 
-def E(x):
-    energy = V_w(x[0]) + 1/2*dot(x[1], x[1])
+def E_k(x):
+    return 1/2*dot(x[1], x[1])
+
+def totalV_i(x):
+    energy = np.zeros_like(x[0, 0])
     for k in range(len(energy)):
         energy[k] += 1/2 * V_i(k, x[0])
-    return np.sum(energy)
+    return energy
 
 def F_w(x0):
     r = sqrt(dot(x0, x0))
