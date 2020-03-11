64de434e5ff72e728c95b1be24ccaa2931957008
Martin Johnsrud <martin.johnsrud@gmail.com>
2020-03-11 15:26:36 +0100

    savex no only saves around 1000 points per particle, as more is unnecessary

--- misc.py
+++ misc.py
@@ -1,21 +1,39 @@
 import numpy as np
+from math import floor
 import os
 
 def savex(xt, T, dt, folder, name):
-    if not os.path.exists("data_" + folder):
-        os.makedirs("data_" + folder + "/")
-    t = len(xt)
+    steps = len(xt)
+    stepSize = floor(steps/1000)
+    # around 1000 points is enough
+    xt = xt[::stepSize]
+    steps = len(xt)
     N = len(xt[0, 0, 0])
-    xt = np.reshape(xt, (t, 4*N))
-    np.savetxt("data_"+folder+"/"+"parametres.csv", [T, dt], delimiter=",")
-    np.savetxt("data_"+folder+"/"+name+".csv", xt)
+    xt = np.reshape(xt, (steps, 4*N))
+
+    path = "data_" + folder + "/"
+    if not os.path.exists(path):
+        os.makedirs(path)
+    
+    header = "Time simulated, number of steps and number of particles"
+    
+    np.savetxt(
+        path + "parametres.csv", 
+        [T, steps, N], 
+        delimiter=",", 
+        header = header)
+        
+    np.savetxt(path + name + ".csv", xt)
 
 def loadx(folder, name):
+    path = path = "data_" + folder + "/" 
     if not os.path.exists("data_"+folder+"/"+name+".csv"):
-        print("Can't load")
-    T, dt = np.loadtxt("data_"+folder+"/"+"parametres.csv", unpack = True)
-    xt = np.loadtxt("data_"+folder+"/"+name+".csv")
-    t = len(xt)
-    N = int(len(xt[0])/4)
+        print("Can't load file")
+        return
+
+    T, steps, N = np.loadtxt(path + "parametres.csv", unpack = True)
+    xt = np.loadtxt(path + name + ".csv")
+    dt = T / steps
+    print("Loaded {} particles with {} steps".format(N, steps))
 
     return np.reshape(xt, (t, 2, 2, N)), T, dt
