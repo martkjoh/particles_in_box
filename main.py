b1c08b4bd089305fcdd89991a2252352ba0f87d8
Martin Johnsrud <martin.johnsrud@gmail.com>
2020-03-12 13:30:46 +0100

    added plot with kin.e. distribution

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
-from physics import sumF, F_w, temp
+from physics import sumF, F_w, temp, sumE, E_k
 from verlet import timeEvolve, allVelToOne, evenVel
 from misc import savex, loadx
 from plot import *
 from matplotlib import pyplot as plt
 from time import sleep
 
-def runSimulation(N, T, dt, name, velDist=evenVel):
+def runSimulation(N, T, dt, name, velDist=evenVel, E = sumE):
     timeSteps = int(T/dt)
     xt = timeEvolve(N, sumF, timeSteps, dt, velDist=velDist)
-    savex(xt, T, name)
+    savex(xt, T, name, E = sumE)
 
-def task1a():
+def task1():
     dts = [0.1, 0.05, 0.01]
-
 
     for dt in dts:
         name = "one_particle_{}".format(dt).replace(".", "")
@@ -24,25 +23,69 @@ def task1a():
         fig, ax = plt.subplots(1, 2, figsize = (14, 7))
         fig.suptitle("$\Delta t = {0:1.3f}$".format(dt))
         plotCircle(ax[0])
-        plotParticlePath(xt, ax[0], 0, color = cm.viridis(0.2))
+        plotParticlePath(xt, ax[0], 0, color = cm.viridis(0.2), title = Position)
         plotRelTotE(E, T, dt, ax[1])
 
         plt.tight_layout()
         plt.savefig("figs/"+name+".png")
 
+def task2():
+    Ns = [2, 5, 10]
+    T = 50
+    fig, ax = plt.subplots(3, 2, figsize = (14, 21))
+    for i in range(len(Ns)):
+        name = "{}_particles".format(Ns[i])
+        
+        # runSimulation(Ns[i], T, 0.01, name)
 
-task1a()
+        xt, E, T, dt, N = loadx(name)
 
-# runSimulation(10, 10, 0.01, "test")
-# xt, T, dt, N = loadx("test")
+        plotCircle(ax[i, 0])
+        for k in range(Ns[i]):
+            plotParticlePath(
+                xt, ax[i, 0], k, 
+                title = "${}$ particles".format(N), 
+                color = cm.viridis(k/N))
+            plotRelTotE(E, T, dt, ax[i, 1])
+        
+    plt.tight_layout()
+    plt.savefig("figs/several_particles.png")
 
-# # Regular plot, all
-# for t in range(10):
-#     fig, ax = plt.subplots()
-#     plotCircle(ax)
-#     for i in range(N):
-#         plotParticlePath(xt[100*t:100*(t+1)], ax, i, color=cm.viridis(i/N))
-#     plt.show()
+def scatter():
+    fig, ax = plt.subplots(figsize = (10, 10))
+    N = 50
+    T = 1000
+    name = "{}_particles_time_{}".format(N, T)
+
+    # runSimulation(N, T, 0.01, name)
+
+    xt, E, T, dt, N = loadx(name)
+
+    title = "${}$ particles, T = ${}$".format(N, T)
+    plotCircle(ax)
+    for k in range(N):
+        plotScatter(xt, k, ax, title = title, color = cm.viridis(k/N))
+        
+    plt.savefig("figs/scatter.png")
+
+def task2c():
+    T = 100
+    N = 50
+    dt = 0.01
+    name = "all_vel_to_one"
+
+    # runSimulation(N, T, dt, name, velDist=allVelToOne, E = E_k)
+
+    xt, E, T, dt, N = loadx(name)
+
+    fig, ax = plt.subplots(2, figsize = (7, 7))
+    plotEDist(E, T, ax)
+
+    plt.tight_layout()
+    plt.savefig("figs/" + name)
+
+task2c()
+
 
 # velDistirbute
 # xAv = np.array([np.einsum("tijk -> tij", xt)]) / N
--- plot.py
+++ plot.py
@@ -17,10 +17,10 @@ def plotCircle(ax):
     ax.plot(R*cos(theta), R*sin(theta))
 
 
-def plotParticlePath(xt, ax, k, color = "b"):
+def plotParticlePath(xt, ax, k, color = "b", title = ""):
     ax.plot(xt[0, 0, 0, k], xt[0, 0, 1, k], "rx")
     ax.plot(xt[:, 0, 0, k], xt[:, 0, 1, k], color = color)
-    ax.set_title("Postion")
+    ax.set_title(title)
     ax.set_xlabel("x")
     ax.set_ylabel("y")
 
@@ -41,3 +41,25 @@ def plotVelDistrib(xt, k, ax):
     vxCont = np.linspace(np.min(vx), np.max(vx), 100)
     f = boltzDist(xt)(vxCont)
     ax.plot(vxCont, f)
+
+def plotScatter(xt, k, ax, color = "b", title = ""):
+    ax.scatter(xt[:, 0, 0, k], xt[:, 0, 1, k], color = color, alpha = 0.1)
+    ax.set_title(title)
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+
+def plotEDist(E, T, ax):
+    N = len(E[0])
+    steps = len(E)
+    t = np.linspace(0, T, steps)
+    EAv = np.einsum("tk -> k", E[900:]) / N
+    ax[0].hist(E[0], bins = 20)
+    ax[0].set_title("$T = 0$")
+    ax[0].set_xlabel("$E$")
+    ax[0].set_ylabel("N")
+    ax[1].hist(E[-1], bins = 20)
+    ax[1].set_title("Average, $T \in [90, 100]$")
+    ax[1].set_xlabel("$E$")
+    ax[1].set_ylabel("N")
+
+    plt.tight_layout()
